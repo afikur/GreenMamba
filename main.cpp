68a8c956f0deeb456aa1e340a3e59dbcd39df8f8
Afikur Rahman <this.afikur@gmail.com>
2017-08-24 19:15:21 +0600

    add color and projection

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <GL/gl.h>
+#include <GL/glut.h>
+
+void display_callback();
+void reshape_callback(int, int);
+
+void init() {
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+}
+
+int main(int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("GreenMamba");
+    glutDisplayFunc(display_callback);
+    glutReshapeFunc(reshape_callback);
+    init();
+    glutMainLoop();
+    return 0;
+}
+
+void display_callback() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+void reshape_callback(int w, int h) {
+    glViewport(0, 0,(GLsizei)w, (GLsizei)h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0, 40.0, 0.0, 40.0, -1.0, 1.0);
+    glMatrixMode(GL_MODELVIEW);
+}
