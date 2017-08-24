62bc0f05448098d5a7e8530eebea516493487a2e
Afikur Rahman <this.afikur@gmail.com>
2017-08-24 20:10:33 +0600

    set fps

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@
 
 #define COLUMNS 40
 #define ROWS 40
+#define FPS 10
 
+void timer_callback(int);
 void display_callback();
 void reshape_callback(int, int);
 
@@ -20,14 +22,22 @@ int main(int argc, char **argv) {
     glutCreateWindow("GreenMamba");
     glutDisplayFunc(display_callback);
     glutReshapeFunc(reshape_callback);
+    glutTimerFunc(0, timer_callback, 0);
     init();
     glutMainLoop();
     return 0;
 }
 
+int index = 0;
+
 void display_callback() {
     glClear(GL_COLOR_BUFFER_BIT);
     drawGrid();
+    glRectd(index, 20, index+2, 22);
+    index++;
+    if(index > 40) {
+        index = 0;
+    }
     glutSwapBuffers();
 }
 
@@ -37,4 +47,9 @@ void reshape_callback(int w, int h) {
     glLoadIdentity();
     glOrtho(0.0, COLUMNS, 0.0, ROWS, -1.0, 1.0);
     glMatrixMode(GL_MODELVIEW);
+}
+
+void timer_callback(int) {
+    glutPostRedisplay();
+    glutTimerFunc(1000/FPS, timer_callback, 0);
 }
